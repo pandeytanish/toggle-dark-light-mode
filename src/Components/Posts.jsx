cb00cb7752411a806756201d35acbb25c94f910c
Tanish Pandey <tanishpandey14@gmail.com>
2024-02-22 08:21:17 +0530

    toggle between dark and light mode using useContext

---
+++ src/Components/Posts.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import { ThemeContext } from '../Context/ThemeContext';
+const Posts = () => {
+  const {theme,handleOnClick} = useContext(ThemeContext);
+  return (
+    <div>
+        <h2>My theme {theme} </h2>
+        <button 
+        className={ `btn ${theme === "dark" ? "btn-light" : "btn-dark"} border `} 
+        onClick={handleOnClick}>
+          {theme === "dark" ? "light" : "dark"} 
+          </button>
+        
+
+    </div>
+  );
+};
+
+export default Posts;
